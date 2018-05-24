7bc52b2c997bfa946dc91eb05cbaec2d6d67c068
Mmouky <33157839+Mmouky@users.noreply.github.com>
2018-05-24 09:42:08 +0200

    Dessins dans les classes boutons spécifiques

--- src/fr/exia/exiawar/boutons/Bouton.java
+++ src/fr/exia/exiawar/boutons/Bouton.java
@@ -1,12 +1,7 @@
 package fr.exia.exiawar.boutons;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.ArrayList;
-
-import javax.swing.ImageIcon;
 
 import fr.exia.exiawar.Interface.IBoutonAction;
 import fr.exia.exiawar.Interface.IDessiner;
@@ -14,9 +9,9 @@ import fr.exia.exiawar.ecran.Affichage;
 
 public abstract class Bouton implements IDessiner, IBoutonAction {
 
-	private int x, y, width, height;
-	private String text;
-	private boolean isFocus, isLocked;
+	protected int x, y, width, height;
+	protected String text;
+	protected boolean isFocus, isLocked;
 
 	protected Affichage affichage;
 
@@ -44,37 +39,6 @@ public abstract class Bouton implements IDessiner, IBoutonAction {
 		this.affichage = affichage;
 		this.isLocked = isLocked;
 		boutons.add(this);
-	}
-
-	public void dessiner(Graphics2D g2d) {
-		if (isLocked == false) {
-			if (isFocus == false) {
-				g2d.setColor(Color.BLACK);
-				g2d.draw(getRect());
-			} else {
-				int thickness = 3;
-				g2d.setColor(Color.BLUE);
-				g2d.fill(new Rectangle(x - thickness, y - thickness, width + thickness * 2, height + thickness * 2));
-				g2d.clearRect(x, y, width, height);
-			}
-		} else {
-			g2d.setColor(Color.BLACK);
-			g2d.draw(getRect());
-
-			int widthCadenas = 19;
-			int heightCadenas = 25;
-
-			g2d.drawImage(new ImageIcon("cadenas.png").getImage(), x - widthCadenas - 5,
-					y + height / 2 - heightCadenas / 2, widthCadenas, heightCadenas, null);
-		}
-
-		g2d.setFont(new Font(Affichage.POLICE, Font.BOLD, 20));
-		String UPtext = text.toUpperCase();
-
-		int widthTxt = (int) g2d.getFontMetrics().getStringBounds(UPtext, g2d).getWidth();
-		int heightTxt = (int) g2d.getFontMetrics().getStringBounds(UPtext, g2d).getHeight();
-
-		g2d.drawString(UPtext, x + width / 2 - widthTxt / 2, y + height / 2 + heightTxt / 3);
 	}
 
 	public int getX() {
--- src/fr/exia/exiawar/boutons/OptionsB.java
+++ src/fr/exia/exiawar/boutons/OptionsB.java
@@ -1,6 +1,11 @@
 package fr.exia.exiawar.boutons;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
 
 import fr.exia.exiawar.ecran.Affichage;
 import fr.exia.exiawar.enums.EnumAffichage;
@@ -23,6 +28,37 @@ public class OptionsB extends Bouton{
 	public void action(Graphics2D g2d) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void dessiner(Graphics2D g2d) {
+		if (isLocked == false) {
+			if (isFocus == false) {
+				g2d.setColor(Color.BLACK);
+				g2d.draw(getRect());
+			} else {
+				int thickness = 3;
+				g2d.setColor(Color.BLUE);
+				g2d.fill(new Rectangle(x - thickness, y - thickness, width + thickness * 2, height + thickness * 2));
+				g2d.clearRect(x, y, width, height);
+			}
+		} else {
+			g2d.setColor(Color.BLACK);
+			g2d.draw(getRect());
+
+			int widthCadenas = 19;
+			int heightCadenas = 25;
+
+			g2d.drawImage(new ImageIcon("cadenas.png").getImage(), x - widthCadenas - 5,
+					y + height / 2 - heightCadenas / 2, widthCadenas, heightCadenas, null);
+		}
+
+		g2d.setFont(new Font(Affichage.POLICE, Font.BOLD, 20));
+		String UPtext = text.toUpperCase();
+
+		int widthTxt = (int) g2d.getFontMetrics().getStringBounds(UPtext, g2d).getWidth();
+		int heightTxt = (int) g2d.getFontMetrics().getStringBounds(UPtext, g2d).getHeight();
+
+		g2d.drawString(UPtext, x + width / 2 - widthTxt / 2, y + height / 2 + heightTxt / 3);
 	}
 
 }
